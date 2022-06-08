2f487d820041521cdc79f345b167a50e4c8ca5f9
20142995 <sheshang5201314@163.com>
2022-06-08 12:18:02 +0800

    commit

--- tools/xray/webhook.py
+++ tools/xray/webhook.py
@@ -7,6 +7,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
 def xray_webhook():
     try:
@@ -16,14 +17,17 @@ def xray_webhook():
     except:
         return 'error'
     if data_type == "web_vuln":
-        info = {'addr':vuln["detail"]["addr"], 'plugin':vuln["plugin"],'payload':vuln["detail"]["payload"]}
+        info = {'addr': vuln["detail"]["addr"],
+                'plugin': vuln["plugin"], 'payload': vuln["detail"]["payload"]}
         print(info)
         try:
-            if os.environ.get('DINGTALK_TOKEN',''):
-                send_text(str(info),token=os.environ['DINGTALK_TOKEN'],secret=os.environ.get('DINGTALK_SECRET',''))
+            if os.environ.get('DINGTALK_TOKEN', ''):
+                send_text(str(info), token=os.environ['DINGTALK_TOKEN'], secret=os.environ.get(
+                    'DINGTALK_SECRET', ''))
         except Exception as e:
             pass
     return 'ok'
 
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000)
+    app.run(host='127.0.0.1', port=5000)
