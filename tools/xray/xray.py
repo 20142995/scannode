0331fd1fd6c26ed29803d83c9f92728e78676225
20142995 <sheshang5201314@163.com>
2022-06-08 11:02:59 +0800

    commit

--- tools/xray/xray.py
+++ tools/xray/xray.py
@@ -28,6 +28,7 @@ def xray_webhook():
         return 'error'
     if data_type == "web_vuln":
         info = {'addr':vuln["detail"]["addr"], 'plugin':vuln["plugin"],'payload':vuln["detail"]["payload"]}
+        print(info)
         try:
             if os.environ.get('DINGTALK_TOKEN',''):
                 send_text(str(info),token=os.environ['DINGTALK_TOKEN'],secret=os.environ.get('DINGTALK_SECRET',''))
