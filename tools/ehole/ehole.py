7b8234adba1ae3c308791139b2041c44a0bd9c67
20142995 <sheshang5201314@163.com>
2022-06-08 13:34:42 +0800

    commit

--- tools/ehole/ehole.py
+++ tools/ehole/ehole.py
@@ -11,7 +11,6 @@ from celery import Celery
 app = Celery('ehole', broker=os.environ['BROKER'], backend=os.environ['BACKEND'], )
 app.conf.update(CELERY_TASK_SERIALIZER = 'json',CELERY_RESULT_SERIALIZER = 'json',CELERY_ACCEPT_CONTENT=['json'],CELERY_TIMEZONE = 'Asia/Shanghai',CELERY_ENABLE_UTC = False,)
 
-
 @app.task
 def run(urls):
     result = []
